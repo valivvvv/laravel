99821e4c98d01ed46ded002494c15860f5724924
Vali <vali@royalsoft.eu>
2017-04-05 18:38:37 +0300

    created a registrationForm class extending formRequest

---
+++ app/Http/Requests/RegistrationForm.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\User;
+use Mail;
+use App\Mail\Welcome;
+
+class RegistrationForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true; // anyone can register
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+        ];
+    }
+
+    public function persist()
+    {
+        $user = User::create([ 
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => bcrypt($this->password)
+        ]);
+
+        auth()->login($user);
+
+        Mail::to($user)->send(new Welcome($user));
+    }
+}
